54b324ceb04be0b6ad003d19fd873cac326fded2
Flight3R <michaal747@gmail.com>
2023-04-26 12:13:44 +0200

    Test loop

--- main.py
+++ main.py
@@ -40,52 +40,57 @@ tx = TxDipole(Position(15, 10), Signal(phase=1, power=400, frequency=frequency))
 # measured object x,y center
 obj_position = Position(5, -30)
 
-phase_error_coef = 0.00
-#############################################################
-#                         SIMULATION
-#############################################################
-object = TxDipole(obj_position, is_reflector=True)
+print("method; phase_err; pos_err")
+for phase_err in np.linspace(0.001, 0.06, 100):
 
-simulate(antennas, tx, object, phase_error_coef)
+    phase_error_coef = phase_err
+    #############################################################
+    #                         SIMULATION
+    #############################################################
+    object = TxDipole(obj_position, is_reflector=True)
 
-#############################################################
-#                           PLOTS
-#############################################################
-for output in glob.glob(f"{os.getcwd()}/*.png"):
-    os.remove(output)
+    simulate(antennas, tx, object, phase_error_coef)
 
-method = [0, 1, 2]
-methods = [
-    'analytic',
-    'regression',
-    'variance'
-]
+    #############################################################
+    #                           PLOTS
+    #############################################################
+    for output in glob.glob(f"{os.getcwd()}/*.png"):
+        os.remove(output)
 
-if True:
-    for mt in method:
-        detection_method = select_detection_method(methods[mt])
-        start = time.time()
-        target_position = detection_method(antennas, tx, obj_position, plot=True)
-        end = time.time()
-        print(f"{' OBJECT DETECTION ' :=^50}")
-        print(f"{'method=': <20}{methods[mt]}")
-        if target_position is not None:
-            print(f"{'target_position=' : <20}{target_position :.5}")
-            print(f"{'position error=' : <20}{calculate_distance(target_position, obj_position) :.5}")
-        else:
-            print(f"{'target_position=' : <20}{'Not found'}")
-        print(f"{'time=' : <20}{end-start :.5}")
+    method = [0, 1, 2]
+    methods = [
+        'analytic',
+        'regression',
+        'variance'
+    ]
 
-if True:
-    for mt in method:
-        start = time.time()
-        target_position = detect_object_phase_increment(methods[mt], antennas, tx, object, phase_error_coef, plot=True)
-        end = time.time()
-        print(f"{' OBJECT DETECTION - phase increment ' :=^50}")
-        print(f"{'method=': <20}{methods[mt]}")
-        if target_position is not None:
-            print(f"{'target_position=' : <20}{target_position :.5}")
-            print(f"{'position error=' : <20}{calculate_distance(target_position, obj_position) :.5}")
-        else:
-            print(f"{'target_position=' : <20}{'Not found'}")
-        print(f"{'time=' : <20}{end-start :.5}")
+    if True:
+        for mt in method:
+            detection_method = select_detection_method(methods[mt])
+            # start = time.time()
+            target_position = detection_method(antennas, tx, obj_position, plot=False)
+            if target_position is not None:
+                print(f"{methods[mt]}; {phase_err}; {calculate_distance(target_position, obj_position)}")
+            # end = time.time()
+            # print(f"{' OBJECT DETECTION ' :=^50}")
+            # print(f"{'method=': <20}{methods[mt]}")
+            # if target_position is not None:
+            #     print(f"{'target_position=' : <20}{target_position :.5}")
+            #     print(f"{'position error=' : <20}{calculate_distance(target_position, obj_position) :.5}")
+            # else:
+            #     print(f"{'target_position=' : <20}{'Not found'}")
+            # print(f"{'time=' : <20}{end-start :.5}")
+
+    # if True:
+    #     for mt in method:
+    #         start = time.time()
+    #         target_position = detect_object_phase_increment(methods[mt], antennas, tx, object, phase_error_coef, plot=False)
+    #         end = time.time()
+    #         # print(f"{' OBJECT DETECTION - phase increment ' :=^50}")
+    #         # print(f"{'method=': <20}{methods[mt]}")
+    #         # if target_position is not None:
+    #         #     print(f"{'target_position=' : <20}{target_position :.5}")
+    #         #     print(f"{'position error=' : <20}{calculate_distance(target_position, obj_position) :.5}")
+    #         # else:
+    #         #     print(f"{'target_position=' : <20}{'Not found'}")
+    #         # print(f"{'time=' : <20}{end-start :.5}")
